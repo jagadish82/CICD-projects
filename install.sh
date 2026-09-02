ece28fc219535b7c115acdad68b0ccf9dd3461a8
root <root@vm1.devrat.local>
2026-09-02 15:16:23 +0530

    dd

--- install.sh
+++ install.sh
@@ -29,15 +29,15 @@ docker --version
 
 # kind Cluster Creation
 
-#curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-#chmod +x kubectl
-#sudo mv kubectl /usr/local/bin/
-#kubectl version --client
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version --client
 
 # Install Kind
 
-#curl -Lo ./kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
-#chmod +x kind
-#sudo mv kind /usr/local/bin/
-#kind version
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
+chmod +x kind
+sudo mv kind /usr/local/bin/
+kind version
 
